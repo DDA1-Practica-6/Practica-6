6fe51f7f2189f9459f05437bfa310f8b47eef7c8
Sebastian Carrera <73044857@ucsm.edu.pe>
2021-10-09 05:06:04 -0500

    Experiencia 2

---
+++ Exp02Practica06/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exp02Practica06.Global" Language="C#" %>
---
+++ Exp02Practica06/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Exp02Practica06
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source = DESKTOP-411OTHH\\SQLEXPRESS;" + "Initial Catalog = VENTAS; Integrated Security = True";
+            string selectSQL = "SELECT * FROM Clientes";
+            SqlConnection conexion = new SqlConnection(connectionString);
+            SqlCommand comando = new SqlCommand(selectSQL, conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            // llenando el dataset
+            DataSet ventas = new DataSet();
+            adapter.Fill(ventas, "clientes");
+            // enlazar el gridview
+            GridView1.DataSource = ventas;
+            GridView1.DataBind();
+        }
+    }
+}
